174f6faeb663304e87cd7a64589b3a9ddcd855e8
Edgar Vivanco <evivancovid@gmail.com>
2022-02-28 17:57:48 -0600

    Separate functions were created for the game in english and in spanish. Management of entering a number has been taken care of. The game prints out the letters that you've already tried.

--- hangman.py
+++ hangman.py
@@ -2,24 +2,14 @@ import os
 import random
 
 
-ALPHA = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-
-def word_generation(language):
-    if language == 1:
-        with open("./data.txt","r",encoding="utf-8") as f:
-            words = [word for word in f]
-
-    elif language == 2:
-        with open("./english_data.txt","r") as f:
-            words = [word for word in f]
-    
-    else:
-        print("Please select a given language")
-
-    selected_word = random.choice(words)
-
-    return selected_word
+def menu():
+     print("""██╗  ██╗ █████╗ ███╗   ██╗ ██████╗ ██╗███╗   ██╗ ██████╗     ███╗   ███╗ █████╗ ███╗   ██╗    
+██║  ██║██╔══██╗████╗  ██║██╔════╝ ██║████╗  ██║██╔════╝     ████╗ ████║██╔══██╗████╗  ██║    
+███████║███████║██╔██╗ ██║██║  ███╗██║██╔██╗ ██║██║  ███╗    ██╔████╔██║███████║██╔██╗ ██║    
+██╔══██║██╔══██║██║╚██╗██║██║   ██║██║██║╚██╗██║██║   ██║    ██║╚██╔╝██║██╔══██║██║╚██╗██║    
+██║  ██║██║  ██║██║ ╚████║╚██████╔╝██║██║ ╚████║╚██████╔╝    ██║ ╚═╝ ██║██║  ██║██║ ╚████║    
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝     ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝    
+                                                                                             """)
 
 
 def hanging_man(trie):
@@ -88,35 +78,132 @@ def hanging_man(trie):
       |
 =========""")
 
-def game (option):
-    os.system('cls' if os.name == 'nt' else 'clear')
 
-    generated_word = word_generation(option)
+def word_generation(language):
+    if language == 1:
+        with open("./data.txt","r",encoding="utf-8") as f:
+            words = [word for word in f]
+
+    elif language == 2:
+        with open("./english_data.txt","r") as f:
+            words = [word for word in f]
+    
+    else:
+        print("Please select a given language")
+
+    selected_word = random.choice(words)
+
+    return selected_word
+
+
+def game_sp(option):
     attempts = 0
-    tries = "6"
+    trys = "6"
     cont = ""
+    generated_word = word_generation(option)
+    letters_tried = []
 
     word_guess = ["_" for letter in generated_word if letter != "\n"]
-    word_reference = [letter for letter in generated_word                 if letter != "\n"]
 
+    word_reference = [letter for letter in generated_word if letter != "\n"]
+
+    os.system('cls' if os.name == 'nt' else 'clear')
 
     while word_guess != word_reference and attempts < 6:
         x = "".join(word_guess)
 
         hanging_man(attempts)
-
-        print(x + "                       You have " + tries + " attempts left")
+        print(x)
         print("\n")
+        print("*********************************************************")
+        print("\n")
+        print(letters_tried)
+        response = input("Adivina una letra: ")
 
+        try:
+            if str.isnumeric(response) == True:
+                raise TypeError
+
+            if response not in word_reference:
+                attempts += 1
+                trys =str(6 - attempts)
+                letters_tried.append(response)
+                letters_tried.sort()
+
+            for i in range(len(word_reference)):
+                if response == word_reference[i]:
+                    word_guess[i] = response
+
+                    if response not in letters_tried:
+                        letters_tried.append(response)
+                        letters_tried.sort()
+
+        except TypeError:
+            con = input("Por favor ingresa una letra (enter para continuar)")
+
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    x = "".join(word_guess)
+
+    if word_guess == word_reference:
+        print ("La palabra era: " + x )
+        print("GANASTE!!!")
+    
+    if attempts == 6:
+        hanging_man(attempts)
+        solution = "".join(generated_word)
+        print("La palabra era: " + solution )
+
+    cont = input("Quieres jugar de nuevo? (s/n): ")
+
+    if cont == "n":
+        return False
+
+
+def game_en(option):
+    attempts = 0
+    tries = "6"
+    cont = ""
+    generated_word = word_generation(option)
+    letters_tried = []
+
+    word_guess = ["_" for letter in generated_word if letter != "\n"]
+
+    word_reference = [letter for letter in generated_word if letter != "\n"]
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    while word_guess != word_reference and attempts < 6:
+        x = "".join(word_guess)
+
+        hanging_man(attempts)
+        print(x)
+        print("\n")
+        print("*********************************************************")
+        print("\n")
+        print(letters_tried)
         response = input("Guess a letter: ")
 
-        if response not in word_reference:
-            attempts += 1
-            tries =str(6 - attempts)
+        try:
+            if str.isnumeric(response) == True:
+                raise TypeError
 
-        for i in range(len(word_reference)):
-            if response == word_reference[i]:
-                word_guess[i] = response
+            if response not in word_reference:
+                attempts += 1
+                tries =str(6 - attempts)
+                letters_tried.append(response)
+                letters_tried.sort()
+
+            for i in range(len(word_reference)):
+                if response == word_reference[i]:
+                    word_guess[i] = response
+
+                    if response not in letters_tried:
+                        letters_tried.append(response)
+                        letters_tried.sort()
+
+        except TypeError:
+            con = input("Please enter a letter (Press enter to continue)")
 
         os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -134,25 +221,25 @@ def game (option):
     cont = input("Do you want to play again? (y/n): ")
 
     if cont == "n":
-        exit()
+        return False
 
 
 def run():
-    while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
+    y = True
 
-        print("""██╗  ██╗ █████╗ ███╗   ██╗ ██████╗ ██╗███╗   ██╗ ██████╗     ███╗   ███╗ █████╗ ███╗   ██╗    
-██║  ██║██╔══██╗████╗  ██║██╔════╝ ██║████╗  ██║██╔════╝     ████╗ ████║██╔══██╗████╗  ██║    
-███████║███████║██╔██╗ ██║██║  ███╗██║██╔██╗ ██║██║  ███╗    ██╔████╔██║███████║██╔██╗ ██║    
-██╔══██║██╔══██║██║╚██╗██║██║   ██║██║██║╚██╗██║██║   ██║    ██║╚██╔╝██║██╔══██║██║╚██╗██║    
-██║  ██║██║  ██║██║ ╚████║╚██████╔╝██║██║ ╚████║╚██████╔╝    ██║ ╚═╝ ██║██║  ██║██║ ╚████║    
-╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝     ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝    
-                                                                                             """)
+    while y == True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        menu()
         option = int(input("""1.- Español
 2.- English
     
 Escoge un Lenguaje || Choose a Language: """))
-        game(option)
+
+        if option == 1:
+            y = game_sp(option)
+
+        if option == 2:
+            y = game_en(option)
 
 if __name__ == '__main__':
     run()
